188c1a66afe2b7fe5727b98a3e2c2cb505835878
MalleshappaVibhuti <Malleshappa.Vibhuti@in.ey.com>
2022-06-17 20:39:32 +0530

    End of Session 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,22 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemssagesComponent } from './memssages/memssages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MemssagesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +33,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/memssages/memssages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memssages',
+  templateUrl: './memssages.component.html',
+  styleUrls: ['./memssages.component.css']
+})
+export class MemssagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
